2c1dfddad5ab858015be29367741d5378c36e47b
Zsc <zsc2333@outlook.com>
2025-02-16 14:40:50 +0800

    update

--- exercises/easy/algorithm5.rs
+++ exercises/easy/algorithm5.rs
@@ -1,14 +1,13 @@
 /*
-	bfs
-	This problem requires you to implement a basic BFS algorithm
+    bfs
+    This problem requires you to implement a basic BFS algorithm
 */
-
 
 use std::collections::VecDeque;
 
 // Define a graph
 struct Graph {
-    adj: Vec<Vec<usize>>, 
+    adj: Vec<Vec<usize>>,
 }
 
 impl Graph {
@@ -21,20 +20,50 @@ impl Graph {
 
     // Add an edge to the graph
     fn add_edge(&mut self, src: usize, dest: usize) {
-        self.adj[src].push(dest); 
-        self.adj[dest].push(src); 
+        self.adj[src].push(dest);
+        self.adj[dest].push(src);
     }
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
-
+        //TODO
         let mut visit_order = vec![];
+        let mut que: VecDeque<usize> = VecDeque::new();
+        // let i: i32;
+        // let j: i32;
+
+        let mut temp: usize = 0;
+        // for i in 0..self.adj.len() {
+        //     if visit_order.contains(&(i as usize)) {
+        //         continue;
+        //     }
+        //     que.push_back(i as usize);
+        //     while !que.is_empty() {
+        //         temp = que.pop_front().unwrap();
+        //         visit_order.push(temp);
+        //         for j in self.adj[temp].iter().cloned() {
+        //             if !visit_order.contains(&(j as usize)) {
+        //                 que.push_back(j as usize);
+        //             }
+        //         }
+        //     }
+        // }
+        que.push_back(start);
+        visit_order.push(start);
+        while !que.is_empty() {
+            temp = que.pop_front().unwrap();
+
+            for j in self.adj[temp].iter().cloned() {
+                if !visit_order.contains(&(j as usize)) {
+                    que.push_back(j as usize);
+                    visit_order.push(j as usize);
+                }
+            }
+        }
+
         visit_order
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -84,4 +113,3 @@ mod tests {
         assert_eq!(visited_order, vec![0]);
     }
 }
-

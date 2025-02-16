98d2924c7c7eb2e24ebfbc530306a76f8ba22aa8
Zsc <zsc2333@outlook.com>
2025-02-16 18:47:50 +0800

    update normal 1

--- exercises/normal/solution2/src/converter.rs
+++ exercises/normal/solution2/src/converter.rs
@@ -1,4 +1,11 @@
+use scanf::scanf;
 pub fn convert_base(num_str: &str, to_base: u32) -> String {
     // TODO: 这里写逻辑
-    todo!()
+    let inv = vec![
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+    ];
+    let (num, base_a) = scanf!(num_str, "{}({})", &str, i32).unwrap();
+    let Ok(num) = i32::from_str_radix(num, base_a);
+    let Ok(res_str) = num.to_string_radix(to_base);
+    res_str
 }

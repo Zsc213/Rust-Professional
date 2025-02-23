e20ead8f0894f74e5b56bcee47ce1c5d4e5ae2c7
Zsc <zsc2333@outlook.com>
2025-02-23 22:57:47 +0800

    update hard 1

--- exercises/hard/solutiont1/src/conjecture.rs
+++ exercises/hard/solutiont1/src/conjecture.rs
@@ -1,3 +1,30 @@
+use primes;
 pub fn goldbach_conjecture() -> String {
-    todo!()
+    let mut res = vec![];
+    let mut count = 2;
+    let mut i = 3;
+    let mut primes_vec = vec![];
+    loop {
+        // 判断i是否为质数
+        if primes::is_prime(i) {
+            primes_vec.push(i);
+        } else {
+            let mut flag = false;
+            for j in 0..primes_vec.len() {
+                let t = (((i - primes_vec[j]) / 2) as f64).sqrt();
+                if t.trunc() == t {
+                    flag = true;
+                }
+            }
+            if !flag {
+                res.push(i.clone());
+                count -= 1;
+                if count == 0 {
+                    break;
+                }
+            }
+        }
+        i += 2;
+    }
+    format!("{},{}", res[0], res[1])
 }

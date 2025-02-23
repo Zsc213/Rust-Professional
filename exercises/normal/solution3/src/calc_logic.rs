ae953c45af499b653ed62cf60517bc5c58dd1df2
Zsc <zsc2333@outlook.com>
2025-02-23 15:21:49 +0800

    update normal 3

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,9 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
     // TODO: 这里写逻辑
-    todo!()
+    let year: f64 = 365.0;
+    let mut res: f64 = 1.0;
+    for i in 0..n {
+        res *= (year - i as f64) / year;
+    }
+    1.0 as f64 - res
 }

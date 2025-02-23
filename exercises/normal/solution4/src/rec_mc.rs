bd4f39490a367221e58fa004ae34773306beb980
Zsc <zsc2333@outlook.com>
2025-02-23 15:44:15 +0800

    update normal 5

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,11 @@
 pub fn dp_rec_mc(amount: u32) -> u32 {
     // TODO: 这里写逻辑
-    todo!()
+    let mut res: u32 = 0;
+    let mut m = amount.clone();
+    let money = vec![100, 50, 30, 20, 10, 5, 2, 1];
+    for i in 0..money.len() {
+        res += m / money[i];
+        m %= money[i];
+    }
+    res
 }
--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,4 +1,17 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
     // TODO: 这里写逻辑
-    todo!()
+    let mut w: Vec<u32> = vec![0, 1];
+    let mut res: u32 = 1;
+    let mut temp: u32;
+    loop {
+        temp = w[0] + w[1];
+        if temp > threshold {
+            break;
+        } else if temp % 2 == 1 {
+            res += temp;
+        }
+        w[0] = w[1];
+        w[1] = temp;
+    }
+    res
 }

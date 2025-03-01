174e7ee520b6d46fa0fc73a37322befe6ef679aa
Zsc <zsc2333@outlook.com>
2025-03-01 14:44:33 +0800

    update hard 4

--- exercises/hard/solutiont4/src/calc_time.rs
+++ exercises/hard/solutiont4/src/calc_time.rs
@@ -1,44 +1,71 @@
 pub fn time_info(time: &str) -> String {
     let month_days: Vec<u32> = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     let month_days_com: Vec<u32> = vec![0, 1, 0, 0, 0, 1, 1, 2, 3, 3, 4, 4];
-    let lunar_year: Vec<u32> = vec![
-        31, 50, 39, 29, 47, 35, 25, 44, 33, 22, 41, 30, 49, 37, 26, 45, 34, 23, 42, 32, 51, 39, 28,
-        47, 36, 24, 44, 33, 23, 41, 30, 48, 37, 26, 45, 35, 24, 42,
-    ];
+
+    // 记录部分新年距1月1日期
+    let new_years = vec![vec![2025, 29], vec![2026, 48]];
+    // 部分假期
+    let vacation = vec![vec![121, 5], vec![357, 10]];
     let mut res_str: Vec<String> = vec![];
     let parts: Vec<String> = time.split('-').map(|s| s.to_string()).collect();
     let year = parts[0].parse::<u32>().unwrap();
     let month = parts[1].parse::<u32>().unwrap();
     let day = parts[2].parse::<u32>().unwrap();
-    let bias = ((year - 1900) * 365 + (year - 1900 - 1) / 4) % 7; // 1月1日偏移
+    let bias = ((year - 1900) * 365 + (year - 1900 - 1) / 4) % 7; //
+                                                                  //println!("bias: {}", bias);
 
     let this_year_is_leap: bool = year % 4 == 0;
 
-    let mut pass_month_days: u32 = bias + (month - 1) * 30 + month_days_com[(month - 1) as usize];
-    if month > 2 && !this_year_is_leap {
-        pass_month_days -= 1;
+    let mut pass_month_days: u32 = (month - 1) * 30 + month_days_com[(month - 1) as usize];
+    if month > 2 && this_year_is_leap {
+        pass_month_days += 1;
     } // 闰年自动填补空缺的一天
-    let mut this_month_bias: u32 = pass_month_days % 7;
-    let n_this_week = (this_month_bias + day) / 7 + 1; // 第几周
-    let n_this_week_t: u32 = (this_month_bias + day + 6) % 7 + 1; // 周几
-    let year_days_pass: u32 = pass_month_days - bias + day; // 本年的第几天
+      //println!("pass month days: {}", pass_month_days);
+
+    let mut this_month_bias: u32 = (pass_month_days + bias) % 7;
+    let mut n_this_week = (pass_month_days + bias + day + 6) / 7; // 第几周
+                                                                  //let n_this_week = (n_this_week - 1) % 5 + 1;
+    let mut n_this_week_t: u32 = (this_month_bias + day + 6) % 7 + 1; // 周几
+    let year_days_pass: u32 = pass_month_days + day; // 本年的第几天
 
     let mut this_year_days_left: u32 = 365 - year_days_pass; // 还剩多少天
     if this_year_is_leap {
         this_year_days_left += 1;
     }
-    let mut days_to_new_year: u32 = 0; // 距离过年
-    if month < 2 {
-        days_to_new_year = 31 - day + 4;
-    } else if month == 2 && day <= 4 {
-        days_to_new_year = day - 4;
-    } else {
-        days_to_new_year = this_year_days_left + 31 + 4;
+
+    if this_year_days_left < 3 {
+        n_this_week = 1;
     }
 
-    let mut A_left = 0;
-    if n_this_week_t == 6 || n_this_week_t == 7 {
-        A_left = 7 - n_this_week_t;
+    let mut days_to_new_year: u32 = 0; // 距离过年
+    for i in 0..new_years.len() {
+        if year == new_years[i][0] {
+            if year_days_pass <= new_years[i][1] {
+                days_to_new_year = new_years[i][1] - year_days_pass;
+            } else {
+                days_to_new_year = new_years[i + 1][1] + this_year_days_left;
+            }
+        }
+    }
+
+    let mut stock_time = 0;
+    if n_this_week_t == 6 || n_this_week_t == 7 || n_this_week_t == 5 {
+        stock_time = 7 - n_this_week_t;
+    }
+    for i in 0..new_years.len() {
+        if year == new_years[i][0]
+            && year_days_pass >= new_years[i][1] - 1
+            && year_days_pass <= new_years[i][1] + 7
+        {
+            stock_time = new_years[i][1] + 7 - year_days_pass - 1;
+        }
+    }
+    for i in 0..vacation.len() {
+        if year_days_pass >= vacation[i][0] - 1
+            && year_days_pass <= vacation[i][0] + vacation[i][1] - 1
+        {
+            stock_time = vacation[i][0] + vacation[i][1] - year_days_pass - 1;
+        }
     }
 
     res_str.push(n_this_week.to_string());
@@ -46,7 +73,7 @@ pub fn time_info(time: &str) -> String {
     res_str.push(year_days_pass.to_string());
     res_str.push(this_year_days_left.to_string());
     res_str.push(days_to_new_year.to_string());
-    res_str.push(A_left.to_string());
+    res_str.push(stock_time.to_string());
 
     res_str.join(",")
 }
--- exercises/hard/solutiont4/src/main.rs
+++ exercises/hard/solutiont4/src/main.rs
@@ -5,7 +5,4 @@ use crate::calc_time::time_info;
 
 mod calc_time;
 
-fn main() {
-    let st = time_info("2025-01-18");
-    println!("{}", st);
-}
+fn main() {}
